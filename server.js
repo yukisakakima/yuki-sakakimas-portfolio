7fd94d212715a1fe6e6e7bf074e0212523184784
yukisakakima <kima87161211@gmail.com>
2023-04-17 12:17:01 +0900

    Implement Node.js integration

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('index.ejs');
+});
+
+app.get('/profile', (req, res) => {
+    res.render('profile.ejs');
+});
+
+app.get('/works', (req, res) => {
+    res.render('works.ejs');
+});
+
+app.get('/yuki_sakakimas_portfolio', (req, res) => {
+    res.render('yuki_sakakimas_portfolio.ejs');
+});
+
+app.listen(3000);
